fc9f5732686f160db7eb1d45968f420360c67134
Shun Cheung <shunkongcheung@gmail.com>
2021-04-24 18:54:11 -0400

    feat(Content/index): cleanup

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -12,63 +12,61 @@ console.log('Must reload extension for modifications to take effect.');
 
 printLine("Using the 'printLine' function from the Print Module");
 
-//  setInterval(async () => {
-//    const nextRestTime = await getNextRestTime();
-//    const current = moment();
-//    console.debug(nextRestTime);
-//    if (nextRestTime >= current) {
-//    }
-//     chrome.action.setBadgeText({ text: nextRestTime.format('HH:mm') });
-// }, 5000);
-// console.log('hey here0');
+const renderModal = (oldBackground) => {
+  // for rendering the modal
+  var head = document.getElementsByTagName('HEAD')[0];
+  var link = document.createElement('link');
+  link.rel = 'stylesheet';
+  link.href =
+    'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css';
+  head.appendChild(link);
 
-// const body = document.body;
-// body.style.background = '#111';
+  const myEle = document.createElement('div');
+  myEle.setAttribute('class', 'reminderModal');
+  document.body.insertBefore(myEle, document.body.firstChild);
+  render(<Popup oldBackground={oldBackground} />, myEle);
+};
 
-// for (let i = 0; i < 200; i++) {
-//   const myEle = document.createElement('div');
-//   myEle.classList.add('snowflake');
+const renderSnow = () => {
+  const body = document.body;
+  const oldBackground = body.style.background;
+  body.style.background = '#111';
 
-//   const size = Math.random() * 2;
-//   myEle.style.width = `${size}vw`;
-//   myEle.style.height = `${size}vw`;
-//   myEle.style.left = `${Math.floor(Math.random() * 100)}vw`;
-
-//   const sec = Math.ceil(Math.random() * 10) + 5;
-//   myEle.style.animation = `snowfall ${sec}s linear infinite`;
-//   myEle.style['animation-delay'] = `-${Math.random() * 2}s`;
-//   myEle.style.setProperty('--left-ini', Math.ceil(Math.random() * 10) + 'vw');
-//   myEle.style.setProperty('--left-end', Math.ceil(Math.random() * 10) + 'vw');
-
-//   if (i % 6 !== 0) {
-//     myEle.style.filter = `blur(1px)`;
-//   }
-
-//   // myEle.style['animation-delay'] = `-${Math.floor(Math.random() * 10)}s`;
-
-//   document.body.insertBefore(myEle, document.body.firstChild);
-//   // render(<Popup />, myEle);
-// }
-
-// console.log('hey here1');
-
-// getNextRestTime().then((nextRestTime) => {
-//   chrome.action.setBadgeText({ text: nextRestTime.format('HH:mm') });
-// });
-// setInterval(async () => {
-//   const nextRestTime = await getNextRestTime();
-//   const current = moment();
-//   console.debug(nextRestTime);
-//   if (nextRestTime >= current) {
-    var head = document.getElementsByTagName('HEAD')[0]; 
-    var link = document.createElement('link');
-    link.rel = 'stylesheet'; 
-    link.href = 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css';
-    head.appendChild(link); 
-
+  for (let i = 0; i < 100; i++) {
     const myEle = document.createElement('div');
-    myEle.setAttribute("class", "reminderModal");
+    myEle.classList.add('snowflake');
+
+    const size = Math.random() * 2;
+    myEle.style.width = `${size}vw`;
+    myEle.style.height = `${size}vw`;
+    myEle.style.left = `${Math.floor(Math.random() * 100)}vw`;
+
+    const sec = Math.ceil(Math.random() * 10) + 5;
+    myEle.style.animation = `snowfall ${sec}s linear infinite`;
+    myEle.style['animation-delay'] = `-${Math.random() * 2}s`;
+    myEle.style.setProperty('--left-ini', Math.ceil(Math.random() * 10) + 'vw');
+    myEle.style.setProperty('--left-end', Math.ceil(Math.random() * 10) + 'vw');
+
+    if (i % 6 !== 0) {
+      myEle.style.filter = `blur(1px)`;
+    }
+
     document.body.insertBefore(myEle, document.body.firstChild);
-    render(<Popup />, myEle);
-//   }
-// }, 5000);
+  }
+
+  return oldBackground;
+};
+
+let oldBackground;
+setInterval(async () => {
+  const nextRestTime = await getNextRestTime();
+  const current = moment();
+
+  // diff in second
+  const diff = (nextRestTime - current) / 1000;
+  if (diff < 55) {
+    oldBackground = renderSnow();
+    renderModal(oldBackground);
+  }
+  console.log('hey here', diff);
+}, 5000);

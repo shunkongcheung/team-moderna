09e756aad1cacfb0267dd13756bbb7b12110db1a
Shun Cheung <shunkongcheung@gmail.com>
2021-04-24 09:45:46 -0400

    feat(Popup): to tsx and clean up

---
+++ src/pages/Popup/Popup.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Popup.css';
+
+const Popup = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <button type="button" className="btn btn-primary">
+          Button 
+        </button>
+      </header>
+    </div>
+  );
+};
+
+export default Popup;

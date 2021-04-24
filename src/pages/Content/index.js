417e4dc0378a940e447c12b0e6c54bb2362c4e1e
Shun Cheung <shunkongcheung@gmail.com>
2021-04-24 15:29:02 -0400

    feat(Content): ffetch data

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -3,6 +3,8 @@ import { render } from 'react-dom';
 
 import { printLine } from './modules/print';
 
+import { getNextRestTime } from '../../logics';
+
 import Popup from './Test';
 
 console.log('Content script works!');
@@ -10,6 +12,13 @@ console.log('Must reload extension for modifications to take effect.');
 
 printLine("Using the 'printLine' function from the Print Module");
 
-const myEle = document.createElement('div');
-document.body.insertBefore(myEle, document.body.firstChild);
-render(<Popup />, myEle);
+setInterval(async () => {
+  const nextRestTime = await getNextRestTime();
+  const current = moment();
+  console.debug(nextRestTime);
+  if (nextRestTime >= current) {
+    const myEle = document.createElement('div');
+    document.body.insertBefore(myEle, document.body.firstChild);
+    render(<Popup />, myEle);
+  }
+}, 5000);

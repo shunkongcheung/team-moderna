c2cf86125e24186c8ff4b347a78b6404650608ab
Shun Cheung <shunkongcheung@gmail.com>
2021-04-24 18:20:03 -0400

    feat(Background): update badge

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -1,2 +1,24 @@
-console.log('This is the background page.');
-console.log('Put the background scripts here.');
+import moment from 'moment';
+import { getNextRestTime } from '../../logics';
+
+setInterval(async () => {
+  const nextRestTime = await getNextRestTime();
+  const current = moment();
+
+  // in seconds
+  const remainTime = (nextRestTime - current) / 1000;
+
+  const hours = Math.floor(remainTime / 3600);
+  const seconds = remainTime % 3600;
+  const minutes = Math.ceil(seconds / 60);
+
+  if (hours > 0) {
+    chrome.action.setBadgeText({ text: `${hours}hr` });
+    chrome.action.setBadgeBackgroundColor({ color: `#ffadad` });
+  } else {
+    chrome.action.setBadgeText({ text: `${minutes}min` });
+    if (minutes > 30)
+      chrome.action.setBadgeBackgroundColor({ color: `#a0c4ff` });
+    else chrome.action.setBadgeBackgroundColor({ color: `#bdb2ff` });
+  }
+}, 1000);

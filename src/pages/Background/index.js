62b2c47cb7f5235a760a2bcc791d245c1c6abed6
Shun Cheung <shunkongcheung@gmail.com>
2021-04-24 18:53:40 -0400

    feat(background): only update when remain > 0

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -8,17 +8,19 @@ setInterval(async () => {
   // in seconds
   const remainTime = (nextRestTime - current) / 1000;
 
-  const hours = Math.floor(remainTime / 3600);
-  const seconds = remainTime % 3600;
-  const minutes = Math.ceil(seconds / 60);
+  if (remainTime > 0) {
+    const hours = Math.floor(remainTime / 3600);
+    const seconds = remainTime % 3600;
+    const minutes = Math.ceil(seconds / 60);
 
-  if (hours > 0) {
-    chrome.action.setBadgeText({ text: `${hours}hr` });
-    chrome.action.setBadgeBackgroundColor({ color: `#ffadad` });
-  } else {
-    chrome.action.setBadgeText({ text: `${minutes}min` });
-    if (minutes > 30)
-      chrome.action.setBadgeBackgroundColor({ color: `#a0c4ff` });
-    else chrome.action.setBadgeBackgroundColor({ color: `#bdb2ff` });
+    if (hours > 0) {
+      chrome.action.setBadgeText({ text: `${hours}hr` });
+      chrome.action.setBadgeBackgroundColor({ color: `#ffadad` });
+    } else {
+      chrome.action.setBadgeText({ text: `${minutes}min` });
+      if (minutes > 30)
+        chrome.action.setBadgeBackgroundColor({ color: `#a0c4ff` });
+      else chrome.action.setBadgeBackgroundColor({ color: `#bdb2ff` });
+    }
   }
-}, 1000);
+}, 5000);

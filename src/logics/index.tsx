c223a54002ee2a39df62d5993c41bf40b8344c24
Shun Cheung <shunkongcheung@gmail.com>
2021-04-24 14:08:27 -0400

    feat(logics): init

---
+++ src/logics/index.tsx
@@ -0,0 +1,5 @@
+import getLastRestTime from './getLastRestTime'
+import getNextRestTime from './getNextRestTime'
+import setRestTime from './setRestTime'
+
+export { getLastRestTime, getNextRestTime, setRestTime }

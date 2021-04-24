56957f189d86d3a19b8170f4cdfda7648ceb98c9
Shun Cheung <shunkongcheung@gmail.com>
2021-04-24 15:23:27 -0400

    feat(setRestTime): use chrome storage

--- src/logics/setRestTime.ts
+++ src/logics/setRestTime.ts
@@ -1,6 +1,6 @@
 const setRestTime = () => {
   const now = new Date();
-  window.localStorage.setItem('REST_TIME', now.toString());
+  chrome.storage.local.set({ REST_TIME: now.toString() });
 };
 
 export default setRestTime;
